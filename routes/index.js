e210a09b95202aba1f8e2efeeaa8a5b763337132
jucaran <jucaran@github.com>
2021-01-15 15:21:05 -0300

    adds mail functionality

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
+const sendMail = require("./mail.controller");
 const router = Router();
 
 router.get("/", (req, res, next) => {
-  res.send("testing");
+  return res.send("testing", req.body);
+});
+
+router.post("/sendmail", async (req, res, next) => {
+  //we try to send the email
+  const [isMailSent, err] = await sendMail(req.body);
+  //if everything is ok we send a 200
+  if (isMailSent) return res.status(200).send("Mail sent!");
+  //Sents error to endpoint
+  else next(err);
 });
 
 module.exports = router;
---
+++ routes/mail.controller.js
@@ -0,0 +1,65 @@
+var nodemailer = require("nodemailer");
+var mg = require("nodemailer-mailgun-transport");
+const { MAILGUN_API_KEY, MAILGUN_DOMAIN } = process.env;
+
+var auth = {
+  auth: {
+    api_key: MAILGUN_API_KEY,
+    domain: MAILGUN_DOMAIN,
+  },
+};
+
+let transporter = nodemailer.createTransport(mg(auth));
+
+const sendMail = async ({ name, email, message }) => {
+  try {
+    let info = await transporter.sendMail({
+      from: `Portfolio, <${email}>`,
+      to: "juan.castro.arancibia@gmail.com",
+      text: `New email from portfolio! 
+        name: ${name}
+        email: ${email},
+        message: ${message}
+      `,
+      html: `
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          </head>
+          <body
+            style="
+              font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande',
+                sans-serif;
+              padding: 2rem;
+              background: #e8e8e8;
+              font-size: 14px;
+            "
+          >
+            <div
+              style="
+                margin: 20px auto;
+                width: 400px;
+                padding: 2rem;
+                border-radius: 5px;
+                box-shadow: 0 0 1px rgba(0, 0, 0, 0.2);
+                background: white;
+                color: black;
+                ">
+              <p>New message from  ${name}!</p>
+              <p>Email:  ${email}!</p>
+              <p>
+                ${message}
+              </p>
+            </div>
+          </body>
+        </html>`,
+    });
+    return [true, info];
+  } catch (err) {
+    return [false, err];
+  }
+};
+
+module.exports = sendMail;
